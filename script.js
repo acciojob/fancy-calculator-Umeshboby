b57c80cecab295466d15b95e3f6889eb0c03cd1c
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-22 10:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+const firstInput = prompt("Enter first number");
+	const multiply =  (firstInput)*2;
+ const secondInput = prompt("Enter second number");
+ const final = multiply + secondInput;
+alert("the final result is"+final );
+
