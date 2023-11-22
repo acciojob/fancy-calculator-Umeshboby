439fd6edae6b7c85e22bdb7a9281f3ddcbdae90a
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-22 10:39:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const firstInput = prompt("Enter first number");
-	const multiply =  (firstInput)*2;
- const secondInput = prompt("Enter second number");
- const final = multiply + secondInput;
-alert("the final result is"+final );
+// const firstInput = prompt("Enter first number");
+// 	const multiply =  (firstInput)*2;
+//  const secondInput = prompt("Enter second number");
+//  const final = multiply + secondInput;
+// alert("the final result is"+final );
 
+// Take input for the first variable and multiply it by 2
+const firstInput = prompt("Enter the first number:");
+const multipliedResult = parseFloat(firstInput) * 2;
+
+// Take input for the second variable
+const secondInput = prompt("Enter the second number:");
+
+// Add the second input to the result of the first input multiplied by 2
+const finalResult = multipliedResult + parseFloat(secondInput);
+
+// Display the final result using alert()
+alert("The final result is: " + finalResult);
